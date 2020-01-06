291461119207dcb52f8ed1567b1f82040b1652ac
dimasm93 <software.dimas_m@icloud.com>
2020-01-06 12:56:10 +0700

    data type

--- basic/swift4-fundamental.playground/Contents.swift
+++ basic/swift4-fundamental.playground/Contents.swift
@@ -1,9 +1,14 @@
 import UIKit;
 
-// data type 
+// data type
 var myAge: Int = 30;
 
 let firstName : String = "Dimas";
 let lastName : String = "Maryanto"
 
 let fullname : String = "\(firstName) \(lastName)";
+let benar: Bool = true;
+
+let bilanganPecahan: Float = 0.4;
+
+let bilanganPecahanDouble: Double = 3.14;
