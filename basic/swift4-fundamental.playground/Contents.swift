321cef61efdebd3966e596e85353601da25e17bf
dimasm93 <software.dimas_m@icloud.com>
2020-01-06 13:32:36 +0700

    condition statement

--- basic/swift4-fundamental.playground/Contents.swift
+++ basic/swift4-fundamental.playground/Contents.swift
@@ -1,3 +1,5 @@
+import UIKit;
+
 // data type
 var myAge: Int = 30;
 
@@ -23,3 +25,23 @@ func getMilk(qty: Int, price: Int = 0) -> Int {
 
 let subTotal = getMilk(qty: 2, price: 10);
 print("Sub Total is Rp.\(subTotal)")
+
+// Conditional Statement
+
+func loveCalculatorScore(yourName: String, theirName: String) -> Int {
+    let loveScore = Int(arc4random_uniform(101));
+    return loveScore;
+}
+
+func descriptionOfScore(loveScore: Int) -> String {
+    if(loveScore >= 80 && loveScore <= 100){
+        return "Your score: \(loveScore), You love each other";
+    } else if (loveScore <= 79 && loveScore >= 40){
+        return "Your score: \(loveScore), Maybe you need communicate each other";
+    } else {
+        return "Your score: \(loveScore), You not love each other";
+    }
+}
+
+let score = loveCalculatorScore(yourName: "Test", theirName: "Test");
+print(descriptionOfScore(loveScore: score));
